21fdf464a7fb0ca45a24c6ed39e769198faeaa30
Hung Pham <hqp001@bucknell.edu>
2024-08-14 05:43:48 -0400

    Add run multiple data seed

--- adversarial_example/run_instance.py
+++ adversarial_example/run_instance.py
@@ -17,27 +17,26 @@ def run_training():
 
     adversarial_data = []
 
-    d_s = 0
-    t_s = 0
-    for n_p in [14, 16, 18]:
-        for n_e, d in [(3, 16), (3, 32), (4, 16), (4, 32)]:
-            model = train(
-                data_seed=d_s,
-                training_seed=t_s,
-                n_pixel_1d=n_p,
-                layer_size=d,
-                n_layers=n_e,
-                test=True,
-                build_only=True,
-            )
+    for d_s, t_s in [(0, 0), (1, 3), (8, 12), (90, 99)]:
+        for n_p in [14, 16, 18]:
+            for n_e, d in [(3, 16), (3, 32), (4, 16), (4, 32)]:
+                model = train(
+                    data_seed=d_s,
+                    training_seed=t_s,
+                    n_pixel_1d=n_p,
+                    layer_size=d,
+                    n_layers=n_e,
+                    test=True,
+                    build_only=True,
+                )
 
-            os.makedirs("./adversarial_example/models", exist_ok=True)
-            torch.save(model.state_dict(), f"./adversarial_example/models/mnist_{n_p}_{n_e}_{d}_{d_s}_{t_s}_dense.pth")
+                os.makedirs("./adversarial_example/models", exist_ok=True)
+                torch.save(model.state_dict(), f"./adversarial_example/models/mnist_{n_p}_{n_e}_{d}_{d_s}_{t_s}_dense.pth")
 
-            for sparsity in [0.5, 0.8, 0.9]:
-                sparse_model = copy.deepcopy(model)
-                sparse_model = prune(sparse_model, n_p, sparsity)
-                torch.save(sparse_model.state_dict(), f"./adversarial_example/models/mnist_{n_p}_{n_e}_{d}_{d_s}_{t_s}_{sparsity}.pth")
+                for sparsity in [0.5, 0.8, 0.9]:
+                    sparse_model = copy.deepcopy(model)
+                    sparse_model = prune(sparse_model, n_p, sparsity)
+                    torch.save(sparse_model.state_dict(), f"./adversarial_example/models/mnist_{n_p}_{n_e}_{d}_{d_s}_{t_s}_{sparsity}.pth")
 
 def get_gurobi_result(gurobi_model):
 
@@ -94,19 +93,20 @@ def add_line_to_csv(file_name, data):
         writer.writerow(data.values())
 
 def argument_generator():
-    for n_p in [14, 16, 18]:
-        for n_e, d in [(3, 16), (3, 32), (4, 16), (4, 32)]:
-            for formulation in ["sos", "bigm"]:
-                for sparsity in [0, 0.5, 0.8, 0.9]:
-                    yield {
-                        'data_seed': 0,
-                        'training_seed': 0,
-                        'n_pixel_1d': n_p,
-                        'formulation': formulation,
-                        'n_layers': n_e,
-                        'layer_size': d,
-                        'sparsity': sparsity
-                    }
+    for d_s, t_s in [(0, 0), (1, 3), (8, 12), (90, 99)]:
+        for n_p in [14, 16, 18]:
+            for n_e, d in [(3, 16), (3, 32), (4, 16), (4, 32)]:
+                for formulation in ["sos", "bigm"]:
+                    for sparsity in [0, 0.5, 0.8, 0.9]:
+                        yield {
+                            'data_seed': 0,
+                            'training_seed': 0,
+                            'n_pixel_1d': n_p,
+                            'formulation': formulation,
+                            'n_layers': n_e,
+                            'layer_size': d,
+                            'sparsity': sparsity
+                        }
 
 def run_formulation():
     for args in argument_generator():
